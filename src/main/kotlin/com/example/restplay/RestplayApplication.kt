92860d926e4dc603a687fab265f65329371d4d0e
developer <developer@digiwave.io>
2022-03-15 14:19:15 +0100

    x509

--- src/main/kotlin/com/example/restplay/RestplayApplication.kt
+++ src/main/kotlin/com/example/restplay/RestplayApplication.kt
@@ -3,39 +3,42 @@ package com.example.restplay
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor
 import org.springframework.security.web.server.SecurityWebFilterChain
+import reactor.core.publisher.Mono
 
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 class RestplayApplication {
 
     @Bean
-    fun reactiveUserDetailsService(): ReactiveUserDetailsService? {
-        // @formatter:off
-        val client: UserDetails = User.withUsername("client")
-            .password("")
-            .roles("USER")
-            .build()
-        // @formatter:on
-        return MapReactiveUserDetailsService(client)
-    }
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        val principalExtractor = SubjectDnX509PrincipalExtractor()
 
-    @Bean
-    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        // @formatter:off
+        principalExtractor.setSubjectDnRegex("OU=(.*?)(?:,|$)")
+
+        val authenticationManager = ReactiveAuthenticationManager { authentication: Authentication ->
+            authentication.isAuthenticated = "Trusted Org Unit" == authentication.name
+            Mono.just(authentication)
+        }
+
         http
-            .x509(withDefaults())
+            .x509 { x509 ->
+                x509
+                    .principalExtractor(principalExtractor)
+                    .authenticationManager(authenticationManager)
+            }
             .authorizeExchange { exchanges ->
                 exchanges
-                    .anyExchange().permitAll()
+                    .anyExchange().authenticated()
             }
         return http.build()
+
     }
 }
 
